9c5b9b29f67ddfb55c86b2c04914306422e6cbb1
Bicer <Bicer@RYUK>
2026-06-03 22:47:43 +0300

    Proje dosyası ekle.

---
+++ sistem_tarihi_saati/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace sistem_tarihi_saati
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(DateTime.Now.ToString());
+                Thread.Sleep(1000);
+
+            }
+        }
+    }
+}
